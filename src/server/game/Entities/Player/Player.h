3a1fc131d71fb2063e534cbd7fcecaac99485d74
Grindelwald <137000133@qq.com>
2017-08-10 10:36:44 +0800

    Update Player.h

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1109,38 +1109,35 @@ class Player : public Unit, public GridObject<Player>
         explicit Player(WorldSession* session);
         ~Player();
 
-        private:
-            bool m_ForgetBGPlayers;
-            bool m_ForgetInListPlayers;
-            uint8 m_FakeRace;
-            uint8 m_RealRace;
-            uint32 m_FakeMorph;
-            public:
-				TeamId m_team;
-				TeamId mFake_team;
-                typedef std::vector<uint64> FakePlayers;
-                void SendChatMessage(const char *format, ...);
-                void FitPlayerInTeam(bool action, Battleground* pBattleGround = NULL);          // void FitPlayerInTeam(bool action, Battleground* bg = NULL);
-                void DoForgetPlayersInList();
-                void DoForgetPlayersInBG(Battleground* pBattleGround);                                          // void DoForgetPlayersInBG(Battleground* bg);
-                uint8 getCFSRace() const { return m_RealRace; }
-                void SetCFSRace() { m_RealRace = GetByteValue(UNIT_FIELD_BYTES_0, 0); }; // SHOULD ONLY BE CALLED ON LOGIN
-                void SetFakeRace(); // SHOULD ONLY BE CALLED ON LOGIN
-                void SetFakeRaceAndMorph(); // SHOULD ONLY BE CALLED ON LOGIN
-                uint32 GetFakeMorph() { return m_FakeMorph; };
-                uint8 getFRace() const { return m_FakeRace; }
-                void SetForgetBGPlayers(bool value) { m_ForgetBGPlayers = value; }
-                bool ShouldForgetBGPlayers() { return m_ForgetBGPlayers; }
-                void SetForgetInListPlayers(bool value) { m_ForgetInListPlayers = value; }
-                bool ShouldForgetInListPlayers() { return m_ForgetInListPlayers; }
-                bool SendBattleGroundChat(uint32 msgtype, std::string message);
-                void MorphFit(bool value);
-                bool IsPlayingNative() const { return GetTeamId() == m_team; }
-                TeamId GetCFSTeamId() const { return m_team; }
-                TeamId GetTeamId() const { return mFake_team != TEAM_NEUTRAL ? mFake_team : (m_bgData.bgTeamId && GetBattleground() ? m_bgData.bgTeamId : m_team); }
-				TeamId GetBgTeamId() const { return mFake_team != TEAM_NEUTRAL ? mFake_team : (m_bgData.bgTeamId ? m_bgData.bgTeamId : GetTeamId()); }
-                bool SendRealNameQuery();
-                FakePlayers m_FakePlayers;
+		private:
+			bool m_ForgetBGPlayers;
+			bool m_ForgetInListPlayers;
+			uint8 m_FakeRace;
+			uint8 m_RealRace;
+			uint32 m_FakeMorph;
+			public:
+				typedef std::vector<uint64> FakePlayers;
+				void SendChatMessage(const char *format, ...);
+				void FitPlayerInTeam(bool action, Battleground* pBattleGround = NULL);
+				void DoForgetPlayersInList();
+				void DoForgetPlayersInBG(Battleground* pBattleGround);
+				uint8 getORace() const { return m_RealRace; }
+				void SetORace() { m_RealRace = GetByteValue(UNIT_FIELD_BYTES_0, 0); }; // SHOULD ONLY BE CALLED ON LOGIN
+				void SetFakeRace(); // SHOULD ONLY BE CALLED ON LOGIN
+				void SetFakeRaceAndMorph(); // SHOULD ONLY BE CALLED ON LOGIN
+				uint32 GetFakeMorph() { return m_FakeMorph; };
+				uint8 getFRace() const { return m_FakeRace; }
+				void SetForgetBGPlayers(bool value) { m_ForgetBGPlayers = value; }
+				bool ShouldForgetBGPlayers() { return m_ForgetBGPlayers; }
+				void SetForgetInListPlayers(bool value) { m_ForgetInListPlayers = value; }
+				bool ShouldForgetInListPlayers() { return m_ForgetInListPlayers; }
+				bool SendBattleGroundChat(uint32 msgtype, std::string message);
+				void MorphFit(bool value);
+				bool IsPlayingNative() const { return GetTeam() == m_team; }
+				uint32 GetOTeam() const { return m_team; }
+				uint32 GetTeam() const { return m_bgData.bgTeam && GetBattleground() ? m_bgData.bgTeam : m_team; }
+				bool SendRealNameQuery();
+				FakePlayers m_FakePlayers;
 		
         void CleanupsBeforeDelete(bool finalCleanup = true);
 
@@ -1209,7 +1206,7 @@ class Player : public Unit, public GridObject<Player>
         PlayerSocial *GetSocial() { return m_social; }
 
         PlayerTaxi m_taxi;
-        void InitTaxiNodesForLevel() { m_taxi.InitTaxiNodesForLevel(getCFSRace(), getClass(), getLevel()); }
+        void InitTaxiNodesForLevel() { m_taxi.InitTaxiNodesForLevel(getORace(), getClass(), getLevel()); }	
         bool ActivateTaxiPathTo(std::vector<uint32> const& nodes, Creature* npc = NULL, uint32 spellid = 1);
         bool ActivateTaxiPathTo(uint32 taxi_path_id, uint32 spellid = 1);
         void CleanupAfterTaxiFlight();
@@ -1259,6 +1256,8 @@ class Player : public Unit, public GridObject<Player>
         void Yell(std::string const& text, const uint32 language);
         void TextEmote(std::string const& text);
         void Whisper(std::string const& text, const uint32 language, uint64 receiver);
+	/// Constructs the player Chat data for the specific functions to use
+	void BuildPlayerChat(WorldPacket* data, uint8 msgtype, std::string const& text, uint32 language) const;
 
         /*********************************************************/
         /***                    STORAGE SYSTEM                 ***/
@@ -2120,6 +2119,7 @@ class Player : public Unit, public GridObject<Player>
         void CheckAreaExploreAndOutdoor(void);
 
         static TeamId TeamIdForRace(uint8 race);
+	TeamId GetTeamId() const { return GetTeam() == ALLIANCE ? TEAM_ALLIANCE : TEAM_HORDE; }
         void setFactionForRace(uint8 race);
 
         void InitDisplayIds();
@@ -2304,8 +2304,6 @@ class Player : public Unit, public GridObject<Player>
                 }
         }
 		
-		void SetBGTeamId(TeamId team) { m_bgData.bgTeamId = team; }
-
         void LeaveBattleground(Battleground* bg = NULL);
         bool CanJoinToBattleground() const;
         bool CanReportAfkDueToLimit();
@@ -2768,6 +2766,7 @@ class Player : public Unit, public GridObject<Player>
         void outDebugValues() const;
         uint64 m_lootGuid;
 
+	uint32 m_team;
         uint32 m_nextSave; // pussywizard
         uint16 m_additionalSaveTimer; // pussywizard
         uint8 m_additionalSaveMask; // pussywizard
